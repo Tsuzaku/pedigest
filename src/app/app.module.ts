3d8aa7f5fc6e8f38e37e383fcfc9ff50cb42e81d
Nhok95 <enrique_1095@hotmail.es>
2019-12-02 15:35:44 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AltaCamareroComponent } from './components/camarerosComponents/alta-camarero/alta-camarero.component';
+// Modules 
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms'; 
+import { HttpClientModule }     from '@angular/common/http'
+import { AppRoutingModule }     from './app-routing.module';
+
+// Components
+import { AppComponent }             from './app.component';
+import { AltaCamareroComponent }    from './components/camarerosComponents/alta-camarero/alta-camarero.component';
 import { ListadoCamareroComponent } from './components/camarerosComponents/listado-camarero/listado-camarero.component';
-import { AltaPedidoComponent } from './components/pedidosComponents/alta-pedido/alta-pedido.component';
-import { ListadoPedidoComponent } from './components/pedidosComponents/listado-pedido/listado-pedido.component';
-import { AltaProductoComponent } from './components/productoComponents/alta-producto/alta-producto.component';
+import { AltaPedidoComponent }      from './components/pedidosComponents/alta-pedido/alta-pedido.component';
+import { ListadoPedidoComponent }   from './components/pedidosComponents/listado-pedido/listado-pedido.component';
+import { AltaProductoComponent }    from './components/productoComponents/alta-producto/alta-producto.component';
 import { ListadoProductoComponent } from './components/productoComponents/listado-producto/listado-producto.component';
-import { EditProductoComponent } from './components/productoComponents/edit-producto/edit-producto.component';
-import { DetallePedidoComponent } from './components/pedidosComponents/detalle-pedido/detalle-pedido.component';
+import { EditProductoComponent }    from './components/productoComponents/edit-producto/edit-producto.component';
+import { DetallePedidoComponent }   from './components/pedidosComponents/detalle-pedido/detalle-pedido.component';
+
+// Services
+import { CategoriaService } from './services/categoria.service';
+import { CamareroService } from './services/camarero.service';
+import { ProductoService } from  './services/producto.service';
 
 @NgModule({
   declarations: [
@@ -26,9 +36,15 @@ import { DetallePedidoComponent } from './components/pedidosComponents/detalle-p
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CategoriaService,
+    CamareroService,
+    ProductoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
