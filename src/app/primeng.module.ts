9a119552abfc821a1239ce76f2d5af5826188bee
Nhok95 <enrique_1095@hotmail.es>
2019-12-04 15:33:24 +0100

    primeng modules

--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
 import { ButtonModule }             from 'primeng/button';
+import { CalendarModule }           from 'primeng/calendar';
+import { DropdownModule }           from 'primeng/dropdown';
+import { InputTextModule }          from 'primeng/inputtext';
+import { CheckboxModule }           from 'primeng/checkbox';
+import { SpinnerModule }            from 'primeng/spinner';
+import { TableModule }              from 'primeng/table';
 
 
 @NgModule({
@@ -10,12 +16,23 @@ import { ButtonModule }             from 'primeng/button';
   imports: [
     CommonModule,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
+    CalendarModule,
+    DropdownModule,
+    InputTextModule,
+    CheckboxModule,
+    SpinnerModule,
+    TableModule
   ],
   exports: [
     BrowserAnimationsModule,
-    ButtonModule
-
+    ButtonModule,
+    CalendarModule,
+    DropdownModule,
+    InputTextModule,
+    CheckboxModule,
+    SpinnerModule,
+    TableModule
   ]
 })
 export class PrimengModule { }
